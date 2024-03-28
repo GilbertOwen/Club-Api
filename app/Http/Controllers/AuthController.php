2fc0a9090cd6e52b2f5ba016d1e076d2f3a9b8cc
Gilbert Owen <moptius@gmail.com>
2024-03-28 13:26:22 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $req)
+    {
+        $validateData = Validator::make($req->all(), [
+            'name' => [
+                'required',
+                'unique:users,name'
+            ],
+            'email' => [
+                'unique:users,email',
+                'required'
+            ],
+            'password' => [
+                'required',
+                'min:8'
+            ]
+        ]);
+
+        if ($validateData->fails()) {
+            return response([
+                'message' => 'Invalid fields',
+                'errors' => $validateData->errors()
+            ], 422);
+        }
+
+        $user = User::create($validateData->validated());
+
+        if (!$user) {
+            return response([
+                'message' => 'Something went wrong with the database, please try again',
+            ], 422);
+        }
+
+        return response([
+            'message' => 'Successfully created a new user',
+        ], 201);
+    }
+    public function login(Request $req){
+        $validateData = Validator::make($req->all(),[
+            'email' => [
+                'required',
+            ],
+            'password' => [
+                'required',
+                'min:8'
+            ]
+        ]);
+
+        if($validateData->fails()){
+            return response([
+                'message' => 'Invalid fields',
+                'errors' => $validateData->errors()
+            ], 422);
+        }
+
+        if(!Auth::attempt($validateData->validated())){
+            return response([
+                'message' => 'Wrong email and password'
+            ], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('sanctum')->plainTextToken;
+
+        return response([
+            'message' => 'Logged in successfully',
+            'user'=> $user,
+            'token' => $token
+        ], 200);
+    }
+    public function logout(Request $request){
+        $user = Auth::user();
+
+        $user->tokens()->delete();
+
+        $request->user()->currentAccessToken()->delete();
+
+        return response([
+            'message' => 'Logged out successfully'
+        ], 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClubController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::prefix('auth')->group(function(){
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+});
+
+Route::prefix('clubs')->middleware('auth:sanctum')->group(function(){
+    Route::get('/', [ClubController::class, 'index']);
+});
