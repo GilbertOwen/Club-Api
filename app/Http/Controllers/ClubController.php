b8cf3365528a941db9c39daaa3fb368dc9561d86
Gilbert Owen <moptius@gmail.com>
2024-03-28 22:50:33 +0700

    feat: added create update and delete method, also added relation to UsersClubs model

--- app/Http/Controllers/ClubController.php
+++ app/Http/Controllers/ClubController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\ClubResource;
 use App\Models\Club;
 use App\Models\User;
 use App\Models\UsersClubs;
 use Illuminate\Http\Request;
+use App\Http\Resources\ClubResource;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\MemberResource;
+use Illuminate\Support\Facades\Validator;
 
 class ClubController extends Controller
 {
@@ -23,10 +25,127 @@ class ClubController extends Controller
 
         return ClubResource::collection($clubs)->additional(['message' => 'Retrieved clubs data']);
     }
+    public function show($id)
+    {
+        $club = Club::find($id);
+
+        if (!$club) {
+            return response([
+                'message' => 'No club found'
+            ], 404);
+        }
+
+        return (new ClubResource($club))->additional(['message' => 'Club successfully retrieved']);
+    }
+    public function store(Request $request)
+    {
+        $validateData = Validator::make($request->all(), [
+            'name' => [
+                'required',
+                'unique:clubs,name'
+            ],
+            'description' => [
+                'required',
+            ],
+            'tech_field' => [
+                'required',
+            ],
+            'club_day' => [
+                'required',
+                'in:Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday'
+            ]
+        ]);
+
+        if ($validateData->fails()) {
+            return response([
+                'message' => 'Invalid fields',
+                'errors' => $validateData->errors()
+            ]);
+        }
+
+        $data = $validateData->validated();
+
+        $club = Club::create($data);
+
+        if (!$club) {
+            return response([
+                'message' => 'Something went wrong, please try again'
+            ]);
+        }
+
+        UsersClubs::create([
+            'user_id' => Auth::id(),
+            'club_id' => $club->id,
+            'is_mentor' => true
+        ]);
+
+        return (new ClubResource($club))->additional(['message' => 'Club created successfully, now youre the mentor']);
+    }
     public function getUserClubs()
     {
         $user = User::find(Auth::id());
 
         return ClubResource::collection($user->clubs)->additional(['message' => "Retrieved user's clubs"]);
+    }
+    public function update(Request $request, $id)
+    {
+        $club = Club::where('id', $id)->first();
+
+        if (!$club) {
+            return response([
+                'message' => 'No club found'
+            ], 404);
+        }
+
+        $rules = [
+            'name' => [
+                'min:3',
+                "unique:clubs,name,$club->id"
+            ],
+            'description' => [
+                '',
+            ],
+            'tech_field' => [
+                'required',
+            ],
+            'club_day' => [
+                '',
+                'in:Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday'
+            ]
+        ];
+
+        $validateData = Validator::make($request->all(), $rules);
+
+        if (!$club->updateOrFail($validateData->validated())) {
+            return response([
+                'message' => 'Failed to update Club'
+            ], 422);
+        }
+
+        return response([
+            'message' => 'success',
+            'club' => $club
+        ], 200);
+    }
+
+    public function delete($id)
+    {
+        $club = Club::find($id);
+
+        if (!$club) {
+            return response([
+                'message' => 'Club not found'
+            ], 404);
+        }
+
+        if (!$club->delete()) {
+            return response([
+                'message' => 'Failed to delete club'
+            ], 422);
+        }
+
+        return response([
+            'message' => 'Successfully deleted club'
+        ]);
     }
 }
--- app/Models/UsersClubs.php
+++ app/Models/UsersClubs.php
@@ -10,4 +10,12 @@ class UsersClubs extends Model
     use HasFactory;
     protected $guarded = ['id'];
 
+    protected $casts = [
+        'is_mentor' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
